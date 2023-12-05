35a293ba30a8d5232841016d66b1e5be97baa40e
Meyre Anusia <meyreanusia@gmail.com>
2023-12-05 10:22:24 -0300

    Form ação

---
+++ src/components/InputOrcamento/index.js
@@ -0,0 +1,15 @@
+import "./InputOrcamento.css";
+function InputOrcamento({onChange, titulo}) {
+  
+  return (
+    <div className="inputOrcamento">
+      <h1>{titulo}</h1>
+      <label htmlFor="codigo">Código</label>
+      <input type="number" name="" id="codigo"  onChange={onChange} placeholder="Digite o código..."/>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" name="nome" id="nome" onChange={onChange} placeholder="Digite o nome..."/>
+    </div>
+  );
+}
+
+export default InputOrcamento;
