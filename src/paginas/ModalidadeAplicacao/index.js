6c65490deb49f26221915e1d9fc5b89d19beb0b6
igabriel39 <gabriel.mendonca2@hotmail.com>
2023-12-13 19:48:52 -0300

    CRUD tabelas de apoio

---
+++ src/paginas/ModalidadeAplicacao/index.js
@@ -0,0 +1,89 @@
+import Tabela from "../../components/Tabela/index.js";
+import Formulario from "../../components/Formulario/index.js";
+import InputOrcamento from "../../components/InputOrcamento/index.js";
+import ButtonCadastrar from "../../components/ButtonCadastrar/index.js";
+import apiModalidadeAplicacao from "../../services/apiModalidadeAplicacao.js";
+import React, { useState, useEffect } from "react";
+import "../Acao/Acao.css";
+
+export default function ModalidadeAplicacao() {
+  // const { handleSubmit, handleBuscar } = apiGrupoDespesa();
+  const [showPopup, setShowPopup] = useState(false);
+  const [codigo, setCodigo] = useState();
+  const [nome, setNome] = useState("");
+  const [dados, setDados] = useState([]);
+  const {  handleEditar, handleExcluir, handleBuscar, handleSubmit } = apiModalidadeAplicacao();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dadosDaApi = await handleBuscar();
+        setDados(dadosDaApi || []);
+      } catch (error) {
+        console.log("Erro ao buscar dados", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  function handleInputs(event) {
+    if (event.target.id === "codigo") {
+      setCodigo(event.target.value);
+
+    }
+    setNome(event.target.value);
+  }
+  const handleAdicionarClick = () => {
+    setShowPopup(true);
+  };
+
+  async function cadastrar(event) {
+    event.preventDefault();
+    const codigoNumber = parseInt(codigo);
+
+    handleSubmit(codigoNumber, nome)
+      .then((resposta) => {
+        if (resposta && resposta.ok) {
+          return resposta.json();
+        } else {
+          console.error("Falha ao criar modalidade aplicação:", resposta);
+          alert(
+            "Erro ao criar Modalidade Aplicação! \n o código já existe na nossa base de dados."
+          );
+          setShowPopup(false);
+          throw new Error("Erro ao criar a Modalidade Aplicação");
+        }
+      })
+      .then((dadoCriado) => {
+        alert("Modalidade Aplicação criada com sucesso");
+        setDados([...dados, dadoCriado]);
+        setShowPopup(false);
+
+      })
+      .catch((error) => {
+        console.error("Erro ao criar modalidade aplicação:", error);
+      });
+  }
+
+  return (
+    <main className={`acao ${showPopup ? "popup-visivel" : ""}`}>
+      <div className="container-tabela">
+        <h1 className="titulo-pagina">Modalidade Aplicação</h1>
+        <Tabela dados={dados} setDados={setDados} handleEditar = {handleEditar} handleExcluir ={handleExcluir} setCodigo ={setCodigo} setNome={setNome} codigo={true}/>
+      </div>
+      <button className="bttAdicionar" onClick={handleAdicionarClick}>
+        +
+      </button>
+      {showPopup && (
+        <Formulario handleSubmit={cadastrar}>
+          <InputOrcamento
+            titulo={"Cadastrar Modalidade Aplicação"}
+            exibirInput={true}
+            onChange={handleInputs}
+          />
+          <ButtonCadastrar />
+        </Formulario>
+      )}
+    </main>
+  );
+}
